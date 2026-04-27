78cc7ac17b19ebd746833e7533ab1b1ca9fb635d
kazanefu <kazanefu12@gmail.com>
2026-04-27 23:28:33 +0900

    separate effect module

--- src/course/course_items/breakable_box/mod.rs
+++ src/course/course_items/breakable_box/mod.rs
@@ -1,6 +1,7 @@
-use bevy_hanabi::prelude::*;
-
 use super::*;
+
+mod brerak_effect;
+use brerak_effect::*;
 
 pub struct BreakableBoxPlugin;
 
@@ -38,8 +39,6 @@ pub fn breakable_box_bundle(x: f32, y: f32, required_speed: f32) -> impl Bundle 
     )
 }
 
-#[derive(Message)]
-struct FireBreakEffect(Vec3);
 
 fn breakable_system(
     mut commands: Commands,
@@ -79,80 +78,4 @@ fn breakable_system(
             }
         }
     }
-}
-
-fn handle_break_effect(
-    mut commands: Commands,
-    effect: Res<BreakEffect>,
-    mut fire_message: MessageReader<FireBreakEffect>,
-) {
-    for position in fire_message.read() {
-        commands.spawn((
-            ParticleEffect::new(effect.0.clone().expect("break effect never setuped")),
-            Transform::from_translation(position.0),
-        ));
-    }
-}
-
-#[derive(Resource, Clone)]
-struct BreakEffect(Option<Handle<EffectAsset>>);
-
-fn setup_break_effect(
-    mut effects: ResMut<Assets<EffectAsset>>,
-    mut break_effect_res: ResMut<BreakEffect>,
-) {
-    let handle = break_effect(&mut effects);
-    break_effect_res.0 = Some(handle);
-}
-
-// break effect
-fn break_effect(effects: &mut Assets<EffectAsset>) -> Handle<EffectAsset> {
-    let mut gradient = bevy_hanabi::Gradient::new();
-    gradient.add_key(0.0, Vec4::new(1.0, 1.0, 0.0, 1.));
-    gradient.add_key(1.0, Vec4::new(1.0,1.0,1.0,0.01));
-
-    let mut module = Module::default();
-
-    let init_pos = SetPositionCircleModifier {
-        center: module.lit(Vec3::ZERO),
-        radius: module.lit(30.0),
-        axis: module.lit(Vec3::Z),
-        dimension: ShapeDimension::Surface,
-    };
-
-    let init_vel = SetVelocityCircleModifier {
-        center: module.lit(Vec3::ZERO),
-        axis: module.lit(Vec3::Z),
-        speed: module.lit(100.),
-    };
-
-    let lifetime = module.lit(1.); // literal value "10.0"
-    let init_lifetime = SetAttributeModifier::new(Attribute::LIFETIME, lifetime);
-    let effect = EffectAsset::new(
-        // Maximum number of particles alive at a time
-        32768,
-        SpawnerSettings::once(100.0.into()),
-        // Move the expression module into the asset
-        module,
-    )
-    .with_name("berak_effect")
-    .init(init_pos)
-    .init(init_vel)
-    .init(init_lifetime)
-    .render(ColorOverLifetimeModifier {
-        gradient: gradient.into(),
-        blend: ColorBlendMode::Modulate,
-        mask: ColorBlendMask::RGBA,
-    })
-    .render(SizeOverLifetimeModifier {
-        gradient: bevy_hanabi::Gradient::from_keys(vec![
-            (0.0, Vec3::splat(15.0)), // 最初大きい
-            (1.0, Vec3::splat(0.0)),  // 消える
-        ])
-        .into(),
-        screen_space_size: false,
-    });
-
-    let effect_asset = effects.add(effect);
-    effect_asset
 }
