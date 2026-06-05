8374f874dc4736a61aac3fd8632b455943114ec9
kazanefu <kazanefu12@gmail.com>
2026-06-06 00:40:00 +0900

    add result transition effect

--- src/materials/game_transition.rs
+++ src/materials/game_transition.rs
@@ -12,7 +12,8 @@ impl Plugin for TransitionMaterialPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugins(Material2dPlugin::<TransitionMaterial>::default())
             .add_systems(Startup, setup)
-            .add_systems(OnEnter(GameState::Playing), start_transition)
+            .add_systems(OnEnter(GameState::Playing), start_playing_transition)
+            .add_systems(OnEnter(GameState::Result), start_result_transition)
             .add_systems(Update, update_transition);
     }
 }
@@ -55,15 +56,25 @@ impl Material2d for TransitionMaterial {
 #[derive(Component)]
 struct GameTransitionEffect;
 
+#[derive(Clone, Copy)]
+enum TransitionDirection {
+    Shrink,
+    Expand,
+}
+
 #[derive(Resource)]
-struct TransitionTimer(Timer);
+struct TransitionState {
+    timer: Timer,
+    color: Color,
+    direction: TransitionDirection,
+}
 
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<TransitionMaterial>>,
 ) {
-    let material = materials.add(TransitionMaterial::from_color(Color::srgb(0.0, 0.6, 1.0)));
+    let material = materials.add(TransitionMaterial::from_color(Color::WHITE));
 
     commands.spawn((
         Mesh2d(meshes.add(Rectangle::new(10000.0, 10000.0))),
@@ -74,38 +85,65 @@ fn setup(
     ));
 }
 
-fn start_transition(mut commands: Commands) {
-    commands.insert_resource(TransitionTimer(Timer::from_seconds(1.2, TimerMode::Once)));
+fn start_transition(commands: &mut Commands, color: Color, direction: TransitionDirection) {
+    commands.insert_resource(TransitionState {
+        timer: Timer::from_seconds(1.2, TimerMode::Once),
+        color,
+        direction,
+    });
+}
+
+fn start_playing_transition(mut commands: Commands) {
+    start_transition(
+        &mut commands,
+        Color::srgb(0.0, 0.6, 1.0),
+        TransitionDirection::Shrink,
+    );
+}
+
+fn start_result_transition(mut commands: Commands) {
+    start_transition(
+        &mut commands,
+        Color::srgb(1.0, 0.0, 1.0),
+        TransitionDirection::Shrink,
+    );
 }
 
 fn update_transition(
     mut commands: Commands,
     time: Res<Time>,
-    timer: Option<ResMut<TransitionTimer>>,
+    transition: Option<ResMut<TransitionState>>,
     effect_query: Query<&MeshMaterial2d<TransitionMaterial>, With<GameTransitionEffect>>,
     mut materials: ResMut<Assets<TransitionMaterial>>,
 ) {
-    let Some(mut timer) = timer else {
+    let Some(mut transition) = transition else {
         return;
     };
 
-    timer.0.tick(time.delta());
+    transition.timer.tick(time.delta());
 
-    let progress = timer.0.fraction();
+    let fraction = transition.timer.fraction();
+
+    let progress = match transition.direction {
+        TransitionDirection::Shrink => fraction,
+        TransitionDirection::Expand => 1.0 - fraction,
+    };
 
     for handle in &effect_query {
         if let Some(mat) = materials.get_mut(handle) {
             mat.params.progress = progress;
+
+            mat.params.base_color = transition.color.to_linear().to_vec4();
         }
     }
 
-    if timer.0.is_finished() {
+    if transition.timer.is_finished() {
         for handle in &effect_query {
             if let Some(mat) = materials.get_mut(handle) {
                 mat.params.progress = 999.0;
             }
         }
 
-        commands.remove_resource::<TransitionTimer>();
+        commands.remove_resource::<TransitionState>();
     }
 }
